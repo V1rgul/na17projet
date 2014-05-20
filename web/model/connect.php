b675f1dbacac25df340e4639e85ad1e5bd0d26b9
V1rgul <V1rgul.42@gmail.com>
2014-05-20 20:22:00 +0200

    phpPgAdmin + Functionnal listeVeterinaire

--- web/model/connect.php
+++ web/model/connect.php
@@ -7,12 +7,42 @@ $PARAM_port='5432';
 $PARAM_nom_bd='dbnf17p165';
 $PARAM_utilisateur='nf17p165';
 $PARAM_mot_passe='Y0dBxcA4';
-$BDD_CONNECTION;
 
 try {
-	$BDD_CONNECTION = new PDO('pgsql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+	$GLOBALS["BDD_CONNECTION"] = new PDO('pgsql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
 	echo('Connection reussie !');
 } catch(Exception $e) {
 	echo('Erreur : '.$e->getMessage().'<br />');
 	echo('N° : '.$e->getCode());
+	die();
 }
+
+
+
+
+
+function constructArrayFromResponse($response, $columns){
+	$r = array();
+	while($line = $response->fetch(PDO::FETCH_OBJ)){
+		$l = array();
+		foreach ($columns as $c) {
+			$l[$c] = $line->$c;
+		}
+		$r[] = $l;
+	}
+	return $r;
+}
+
+function selectFromColumns($table, $columns){
+	try {
+		$response=$GLOBALS["BDD_CONNECTION"]->query("SELECT ".implode(",", $columns)." FROM ".$table);
+		return constructArrayFromResponse($response, $columns);
+	} catch(Exception $e) {
+		echo('Erreur : '.$e->getMessage().'<br />');
+		echo('N° : '.$e->getCode());
+		die();
+	}	
+}
+
+
+
