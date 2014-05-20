67d69ec2b10b7034dba298abe0e279072b6764eb
V1rgul <V1rgul.42@gmail.com>
2014-05-20 19:00:39 +0200

    Correction erreur + Commentaire

--- web/model/connect.php
+++ web/model/connect.php
@@ -1,14 +1,16 @@
 <?php
 
+// Usage : require_once("connect.php");
+
 $PARAM_hote='172.22.0.204'; // tuxa.sme.utc
 $PARAM_port='5432';
 $PARAM_nom_bd='dbnf17p165';
 $PARAM_utilisateur='nf17p165';
 $PARAM_mot_passe='Y0dBxcA4';
-$connexion;
+$BDD_CONNECTION;
 
 try {
-	$connexion = new PDO('pgsql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+	$BDD_CONNECTION = new PDO('pgsql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
 	echo('Connection reussie !');
 } catch(Exception $e) {
 	echo('Erreur : '.$e->getMessage().'<br />');
