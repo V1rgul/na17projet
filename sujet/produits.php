0946058fcc7d923de34e7c1841774090343a26ae
YANG Qiao <yangqiao0505@me.com>
2014-05-26 23:13:06 +0200

    move to the right place...

--- sujet/produits.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("connect.php");
-require_once("requetes.php");
-$idOrdonnaces;
-$columns = Array("id_ordonnances","id_veterinaire");
-
-$query= getProduitGBOrdonnances($idOrdonnaces);
-$ordonnancesAnimal= execQuery($query, $columns);
