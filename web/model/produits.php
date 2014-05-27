d04747360c2f828d9dd0f9bef992a686d33efe1a
Sylveurdance <sylvain.tiset@gmail.com>
2014-05-27 12:56:44 +0200

    Update produits.php

--- web/model/produits.php
+++ web/model/produits.php
@@ -1,6 +1,5 @@
 <?php
 require_once("connect.php");
-<<<<<<< HEAD
 include "requetes.php";
 
 $table = "Produit";
@@ -15,11 +14,3 @@ $ligne1 = execQuery($query1, $columns1);
 $ligne2 = execQuery($query2, $columns1);
 
 ?>
-=======
-require_once("requetes.php");
-$idOrdonnaces;
-$columns = Array("id_ordonnances","id_veterinaire");
-
-$query= getProduitGBOrdonnances($idOrdonnaces);
-$ordonnancesAnimal= execQuery($query, $columns);
->>>>>>> 0946058fcc7d923de34e7c1841774090343a26ae
