c93c98d3056e549a603b921c6b6a9ba66062dba9
YANG Qiao <yangqiao0505@me.com>
2014-06-09 19:40:16 +0200

    fix rel produit facture

--- web/vue/afficher.php
+++ web/vue/afficher.php
@@ -52,7 +52,7 @@ function modifListe($data,$targetModif,$detail,$targetDetail,$id_parent){
     }
     echo "<table border='1'>\n";
     echo "<thead>\n";
-    echo "<tr><th><a href='".$targetModif.".php?id=".$id_parent."&op=ajouter' class='add'><span class='icons'>a</span></a></th>\n";
+    echo "<tr><th><a href='".$targetModif.".php?id=".$id_parent."&id_parent=$id_parent&op=ajouter' class='add'><span class='icons'>a</span></a></th>\n";
     foreach($data[0] as $key => $val)
     {
         echo "<th>$key</th>\n";
