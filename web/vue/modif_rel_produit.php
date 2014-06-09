e79b726e061288bcf1889ba9d41fa981bbc4a609
YANG Qiao <yangqiao0505@me.com>
2014-06-09 19:37:02 +0200

    fix rel produit facture

--- web/vue/modif_rel_produit.php
+++ web/vue/modif_rel_produit.php
@@ -36,9 +36,10 @@ else{
 		}
 ?>
 		<form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
-			nom_produit: <?php echo $nom;?>		
+			nom_produit: <?php echo $nom;?> <br>	
 			remise: <input type="text" name="remise" value="<?php if ($op=='modifier') echo $remise ?>"><br>
 			quantite: <input type="number" name="quantite" value="<?php if ($op=='modifier') echo $quantite ?>"><br>
+			<input type="hidden" name='nom' value="<?php echo $nom;?>">
 			<input type="hidden" name='id_facture' value="<?php echo $id_facture;?>">
 			<input type="hidden" name='op' value="<?php echo $op;?>">
 			<input type="submit" value="Send">
