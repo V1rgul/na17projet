df45f6a9aa871ef33d36ae45408a2284a591588b
YANG Qiao <yangqiao0505@me.com>
2014-06-10 23:09:05 +0200

    update facture a partir d'un produit ordonnance

--- web/vue/modif_produitsOrd.php
+++ web/vue/modif_produitsOrd.php
@@ -8,13 +8,16 @@ if(isset($_POST['nom'])&&!empty($_POST['nom'])){
 	$nom_produit=$_POST['nom'];
 	$id_ordonnance=$_POST['id_ordonnance'];
 	$quantite=$_POST['quantite'];
+	$id_facture=$_POST['id_facture'];
 
 	if ($_POST['op']=='modifier') {
 		updateProduitOrdonnance($nom_produit, $id_ordonnance, $quantite);
+		copyFromPrescriptionsToFacture($nom_produit, $id_facture, $quantite);
 		operationSuccess();
 	}
 	else if ($_POST['op']=='ajouter') {
 		addProduitOrdonnance($nom_produit, $id_ordonnance, $quantite);
+		copyFromPrescriptionsToFacture($nom_produit, $id_facture, $quantite){
 		operationSuccess();
 	}
 }
@@ -42,6 +45,7 @@ else{
 				nom de produit: <?php echo $nom?><br>
 				<input type="hidden" name='nom' value="<?php echo $nom;?>">
 			<?php endif;?>
+			id_facture:<input type="number" name="id_facture" ><br>
 			quantite: <input type="number" name="quantite" value="<?php if ($op=='modifier') echo $quantite ?>"><br>
 			prix_unitaire: <<?php echo $prix_unitaire ?>><br>
 			<input type="hidden" name='op' value="<?php echo $op;?>">
