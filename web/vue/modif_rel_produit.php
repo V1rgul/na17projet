ba9725d8079543fb8eb042bb69089561fd5569b5
YANG Qiao <yangqiao0505@me.com>
2014-06-11 14:39:27 +0200

    fix

--- web/vue/modif_rel_produit.php
+++ web/vue/modif_rel_produit.php
@@ -13,13 +13,24 @@ if(isset($_POST['nom'])&&!empty($_POST['nom'])){
 
 	if ($_POST['op']=='modifier') {
 		updateRel_facture_produit($nom, $id_facture, $remise, $quantite);
-		
 		operationSuccess();
 	}
 	else if ($_POST['op']=='ajouter') {
 		addRel_facture_produit($nom, $id_facture, $remise, $quantite);
 		operationSuccess();
 	}
+}else if(isset($_POST['id_ordonnance'])&&!empty($_POST['id_ordonnance'])){
+	
+	$id_facture=$_POST['id_facture'];
+	$remise=$_POST['remise'];
+	$quantite=$_POST['quantite'];
+	$id_ordonnance=$_POST['id_ordonnance'];
+
+	
+	addRel_facture_produit($nom, $id_facture, $remise, $quantite);
+	copyFromPrescriptionsToFacture($id_ordonnance, $id_facture, $remise);
+	operationSuccess();
+	
 }
 else{
 	$nom=$_GET['id'];
@@ -35,16 +46,20 @@ else{
 			$remise=$data['remise'];
 			$quantite=$data['quantite'];
 		}	
-		else if($op=='ajouter'){
-		}
+		
+
+		if($op=='ajouter'){
 ?>
 		<form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
 			id_ordonnance: <input type="number" name="id_ordonnance" ><br>
-			remise: <input type="text" name="remise" value="<?php if ($op=='modifier') echo $remise ?>"><br>
+			remise: <input type="text" name="remise"><br>
 			<input type="hidden" name='op' value="<?php echo $op;?>">
 			
 			<?php controlesPopup() ?>
 		</form>
+<?php
+		}
+?>
 		<form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
 				id facture: <?php echo $id_facture; ?> <br>
 			<?php if ($op=='ajouter'):?>
